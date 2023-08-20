12d6ba215d988525102e396737d2b5b9d38bee3c
ikaro-souza <ikaro.lisarb@gmail.com>
2023-08-19 22:03:25 -0300

    Revert "adiciona cloudflare function"
    This reverts commit c21095c6ff6352e25b306db4a35b5e6613dc0066.

--- functions/api/manufacturers.ts
+++
@@ -1,7 +0,0 @@
-import { EventContext } from "@cloudflare/workers-types";
-
-export const onRequestGet = (context: EventContext<any, any, any>) => {
-    const url = new URL(context.request.url);
-    const params = url.searchParams;
-    return new Response(params.toString());
-};
